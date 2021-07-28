28a9f66ab145c85a4c3fb8ee23b2a4f30012b8c6
Ahmed Khaled <shadowspedicey@gmail.com>
2021-07-28 08:11:01 +0200

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,20 @@ module.exports = {
 		path: path.resolve(__dirname, "dist"),
 	},
 	devtool: "inline-source-map",
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: [
+							["@babel/preset-env", { targets: "defaults" }]
+						],
+					}
+				}
+			}
+		]
+	}
 };
