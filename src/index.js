1bf2c7908e72af34d266272b35c0dfd09426d06b
Ahmed Khaled <shadowspedicey@gmail.com>
2021-08-24 21:24:53 +0200

    firebase init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
 import isMobile from "is-mobile";
 import DarkMode from "./dark-mode.js";
+
+const firebaseConfig = 
+{
+	apiKey: "AIzaSyBHdHdJ8rRnhiUlyMKlGpT6-yCla0dZEGY",
+	authDomain: "todo-list-3d3e4.firebaseapp.com",
+	projectId: "todo-list-3d3e4",
+	storageBucket: "todo-list-3d3e4.appspot.com",
+	messagingSenderId: "1016517839578",
+	appId: "1:1016517839578:web:daf2dc32b1bf08c5cc73e3",
+	measurementId: "G-SBSFQDWGCG"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 (() =>
 {
